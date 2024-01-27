035703d84053018e59aa35a3a1d349bcce54f7be
SajedaJomaa <97788762+SajedaJomaa@users.noreply.github.com>
2024-01-27 13:50:29 +0200

    Create cart.controller.js

---
+++ src/modules/cart/Controller/cart.controller.js
@@ -0,0 +1,56 @@
+import cartModel from "../../../DB/cart.model.js"
+
+
+export const createCart = async (req,res,next)=>{
+    const {productId,quantity} = req.body;
+   
+    const cart = await cartModel.findOne({userId:req.user._id});
+     if(!cart){
+        const newCart = await cartModel.create({
+            userId:req.user._id,
+            products:{productId,quantity}
+        })
+        return res.status(201).json({message:"success",newCart});
+    }
+    let matchedProduct = false;
+    for(let i=0;i<cart.products.length;i++){
+        if(cart.products[i].productId == productId){
+            cart.products[i].quantity = quantity;
+            matchedProduct = true;
+            break;
+        }else{
+
+        }
+
+    }
+    if(matchedProduct){
+        cart.products.push({productId,quantity});
+    }
+    await cart.save();
+    return res.status(201).json({message:"success",cart});
+}
+
+export const removeItem = async(req,res,next)=>{
+    const {productId} = req.body;
+    await cartModel.updateOne({userId:req.body._id},{
+        $pull:{
+            products:{
+                productId
+            }
+        }
+    })
+    return res.status(200).json({message:"success"});
+}
+
+
+export const clearCart = async(req,res,next)=>{
+    const clearCart = await cartModel.updateOne({userId:req.user._id},{
+        products:[]},
+    )
+    return res.status(200).json({message:"success"});
+}
+
+export const getCart = async(req,res,next)=>{
+    const cart = await cartModel.findOne({userId:req.user._id});
+    return res.status(200).json({message:"success",cart:cart});
+}
